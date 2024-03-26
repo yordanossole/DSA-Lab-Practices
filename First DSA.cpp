6681a85f9ea50fe43121c868c3a80b4519866a86
Yordanos Solomon <117822242+yordanossole@users.noreply.github.com>
2024-03-26 15:18:40 +0300

    Update First DSA.cpp

--- First DSA.cpp
+++ First DSA.cpp
@@ -1,4 +1,5 @@
 //First day DSA Lab session - 4/7/2016 E.C
+//Array and Structures Exercises 
 //1.	Write a program that asks the user to type 10 integers of an array. The program must compute and write how many integers are greater than or equal to 10.
 #include <iostream>
 #include <conio.h>
@@ -75,18 +76,159 @@ int main()
         cin >> a[i];
         cout << "\nEnter the number for b[]: ";
         cin >> b[i];
-        system("cls");
-    }
-    for(int i=0; i<8; i++){
-        if(i<4){
-            c[i] = a[i];
-        }
+        c[i] = a[i];
         c[i+4] = b[i];
-
+        system("cls");
+        
     }
+
     cout << "\nThe elements of c which is a + b: " ;
     for(int i=0; i<8; i++){
         cout << c[i] << " ";
     }
     return 0;
 }
+//Function Exercises
+/*4.	Write a C++ program that will display the calculator menu. The program will prompt the 
+user to choose the operation choice (from 1 to 5). Then it asks the user to input two integer 
+vales for the calculation. See the sample below.
+MENU 
+1. Add 
+2. Subtract 
+3. Multiply 
+4. Divide 
+5. Modulus 
+Enter your choice: 1 
+Enter your two numbers: 12 15 
+Result: 27 
+Continue? Y
+The program also asks the user to decide whether he/she wants to continue the operation. 
+If he/she input ‘y’, the program will prompt the user to choose the operation gain. 
+Otherwise, the program will terminate.
+*/
+#include <iostream>
+
+using namespace std;
+
+int add(int n1, int n2){
+    return n1 + n2;
+}
+int sub(int n1, int n2){
+    return n1 - n2;
+}
+int mul(int n1, int n2){
+    return n1 * n2;
+}
+float div(float n1, int n2){
+    return n1 / n2;
+}
+int mod(int n1, int n2){
+    return n1 % n2;
+}
+
+int main()
+{
+    int choice, num1, num2;
+    char ch;
+    cout << "Choose one of the options: " << endl
+         << "Menu" << endl
+         << "1. Add" << endl
+         << "2. Subtract" << endl
+         << "3. Multiply" << endl
+         << "4. Divide" << endl
+         << "5. Modulus" << endl;
+    do{
+        cout << "\nEnter your choice: ";
+        cin >> choice;
+        cout << "Enter your two numbers (using space): ";
+        cin >> num1 >> num2;
+        switch (choice){
+        case 1:
+            cout << "Result: " << add(num1, num2) << endl;
+            break;
+        case 2:
+            cout << "Result: " << sub(num1, num2) << endl;
+            break;
+        case 3:
+            cout << "Result: " << mul(num1, num2) << endl;
+            break;
+        case 4:
+            cout << "Result: " << div((float)num1, num2) << endl;
+            break;
+        case 5:
+            cout << "Result: " << mod(num1, num2) << endl;
+            break;
+        default:
+            cout << "Invalid choice!!!" << endl;
+        }
+        cout << "Do you want to continue? (Y/y or N/n) ";
+        cin >> ch;
+    }while(ch == 'y' || ch == 'Y');
+    return 0;
+}
+
+/*5.	Write a program that will ask the user to input three integer values
+from the keyboard. Then it will print the smallest and largest of those numbers
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[3];
+
+    cout << "Enter the 3 numbers using space: ";
+    for(int i=0; i<3; i++){
+        cin >> arr[i];
+    }
+    int MIN = arr[0], MAX = arr[0];
+    for(int i=0; i<3; i++){
+        if(arr[i] < MIN){
+            MIN = arr[i];
+        }
+        if(arr[i] > MAX){
+            MAX = arr[i];
+        }
+    }
+    cout << "The min is: " << MIN << "\nThe max is: " << MAX;
+
+
+    return 0;
+}
+/*
+6.	Write a C++ program to pass an array containing age of person to a function. 
+This function should find average age and display the average age in main function.
+*/
+//Pointer Exercise
+//7.	Write block of code that take an input from the user and display the same value using pointer.
+
+
+//8.	Write a program that asks the user to enter integers as inputs to be stored in the 
+//variables 'a' and 'b' respectively. There are also two integer pointers named ptrA and ptrB. 
+//Assign the values of 'a' and 'b' to ptrA and ptrB respectively, and display them.
+
+
+//9.	Write a C++ program to accept five integer values from keyword and print the elements 
+//of the array in reverse order using a pointer.
+
+
+//10.	Write a function countEven(int*, int) which receives an integer array and its size, 
+//and returns the number of even numbers in the array.
+
+
+//11.	Write two functions void cubeByPtr1 ( int * numPtr ) and void cubeByPtr2 ( int & numPtr ) 
+//which returns a cube of a number.
+
+
+
+
+
+
+
+
+
+
+
+
+
