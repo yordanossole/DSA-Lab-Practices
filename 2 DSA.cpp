44435e16b9d2e28f09e6334c57ee5550fb086d2b
Yordanos Solomon <117822242+yordanossole@users.noreply.github.com>
2024-04-22 13:36:56 +0300

    Update 2 DSA.cpp

--- 2 DSA.cpp
+++ 2 DSA.cpp
@@ -1,7 +1,14 @@
+#include <iostream>
+
+using namespace std;
 /*
 2.	Show how the insertion sort algorithm will sort the list: 5, 12, 25, 31, 15, 6, 8, 21.
 A.	How many comparisons will be required?
 B.	How many comparisons will be required in case of bubble sort?
+
+3.	Show how the selection sort algorithm will sort the list: 5, 12, 25, 31, 15, 6, 8, 21. 
+A.	How many comparisons will be required? 
+B.	How many comparisons will be required in case of bubble sort? 
 */
 int insertionSort (int nums[], int len) {
     int key;
@@ -22,7 +29,7 @@ int insertionSort (int nums[], int len) {
         }
     }
     return counter;
-} // 19 comparisons
+}
 
 int bubbleSort (int nums[], int len) {
     int counter = 0;
@@ -37,4 +44,46 @@ int bubbleSort (int nums[], int len) {
         }
     }
     return counter++;
-} // 12 comparisons
+}
+int selectionSort (int nums[], int len) {
+    int counter = 0;
+
+    for (int i=0; i<len-1; i++) {
+        int minIndex = i;
+        for (int j=i+1; j<len; j++) {
+            if (nums[j] < nums[minIndex]) {
+                minIndex = j;
+            }
+            counter++;
+        }
+        int temp = nums[minIndex];
+        nums[minIndex] = nums[i];
+        nums[i] = temp;
+    }
+    return counter;
+}
+
+
+int main () {
+    int nums[] = {5, 12, 25, 31, 15, 6, 8, 21};
+    for (int i=0; i<8; i++) {
+        cout << nums[i] << " ";
+    }
+
+    // int counter = insertionSort (nums, 8); // 19
+    // int counter = bubbleSort (nums, 8); // 12
+    int counter = selectionSort (nums, 8); // 28
+
+    cout << endl;
+    for (int i=0; i<8; i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+    cout << "No of comparison: " << counter;
+    return 0;
+}
+/*
+3.	Show how the selection sort algorithm will sort the list: 5, 12, 25, 31, 15, 6, 8, 21. 
+A.	How many comparisons will be required? 
+B.	How many comparisons will be required in case of bubble sort? 
+*/
